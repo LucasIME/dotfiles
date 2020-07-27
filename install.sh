785775300c12f5fd18287fc5fe7b65dfd6b4cc50
Lucas Meireles <meireles31@msn.com>
2020-07-27 18:18:54 +0100

    Add autojump to install script

--- install.sh
+++ install.sh
@@ -68,6 +68,7 @@ install_oh_my_zsh() {
 install_on_my_zsh_plugins() {
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    try_install autojump
 }
 
 ### Main ###

5e8bad0211a173982d1aff61de1807b16170aed6
Lucas Meireles <meireles31@msn.com>
2025-03-24 20:43:19 +0000

    fix fzf install

--- install.sh
+++ install.sh
@@ -81,7 +81,7 @@ install_fzf() {
     else
         echo "Installing fzf through git"
         git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-        ~/.fzf/install --all
+        ~/.fzf/install
     fi
 }
 

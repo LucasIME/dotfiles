21122a074924c5daad1266dc255f6881746b0295
Lucas Meireles <meireles31@msn.com>
2026-04-21 11:49:38 +0100

    update tree sitter setup

--- vim/.config/nvim/lua/lmeireles/plugins.lua
+++ vim/.config/nvim/lua/lmeireles/plugins.lua
@@ -2,33 +2,41 @@ local lazy = require("lazy")
 
 local treesitterObject = {
   "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    "romgrk/nvim-treesitter-context", -- Shows context that you are at the top
-  },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "bash",
-        "c",
-        "lua",
-        "luadoc",
-        "vim",
-        "vimdoc",
-        "query",
-        "elixir",
-        "eex",
-        "heex",
-        "javascript",
-        "html",
-        "typescript",
-        "python",
-        "java",
-        "go",
-        "rust",
-      },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
+  branch = 'main',
+  init = function()
+    local ensureInstalled = {
+      "bash",
+      "c",
+      "lua",
+      "luadoc",
+      "vim",
+      "vimdoc",
+      "query",
+      "elixir",
+      "eex",
+      "heex",
+      "javascript",
+      "html",
+      "typescript",
+      "python",
+      "java",
+      "go",
+      "rust",
+    }
+    local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+    local parsersToInstall = vim.iter(ensureInstalled)
+        :filter(function(parser)
+          return not vim.tbl_contains(alreadyInstalled, parser)
+        end)
+        :totable()
+    require('nvim-treesitter').install(parsersToInstall)
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function()
+        -- Enable treesitter highlighting and disable regex syntax
+        pcall(vim.treesitter.start)
+        -- Enable treesitter-based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
   end,
   build = ":TSUpdate",
